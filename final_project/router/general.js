60d2baa8bb955cbbd6265c53a2b7c181516662dd
CK <cklau00@gmail.com>
2025-09-16 01:40:07 -0400

    added isbn code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,42 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4));
+
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let book = books[isbn];
+
+  if (book) {
+    res.send(JSON.stringify(book, null, 4));
+  } else {
+    return res.status(404).json({message: 'Unable to find book with ISBN=' + isbn});
+  }
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  let author = req.params.author;
+  let target_books = {};
+
+  let booklist = Array.from(books);
+  console.log(books);
+
+  
+
+  if (booklist) {
+    res.send(JSON.stringify(booklist, null, 4));
+  } else {
+    return res.status(404).json({message: 'Unable to find book written by ' + author});
+  }
+
 });
 
 // Get all books based on title
